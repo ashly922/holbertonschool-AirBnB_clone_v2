fed2abb5e2cc91a86e20a9bf212ac456a01022c6
ashly922 <ashly922@yahoo.com>
2023-07-17 22:06:51 -0500

    documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 
 app.route('/', strict_slashes=False)
 def hello_route():
+    """
+    Route handler for the root URL '/'. Returns a greeting message.
+
+    Returns:
+        str: A string containing the greeting message.
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
+     """
+    Main entry point of the application.
+    Starts the Flask web server.
+
+    Note:
+        This section will only execute when the script is directly run, not when it is imported as a module.
+    """
     app.run(host='0.0.0.0', port=5000)
