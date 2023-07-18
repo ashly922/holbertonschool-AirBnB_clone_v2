06bb06486db98d48393af1643002e6ab4f602892
ashly922 <ashly922@yahoo.com>
2023-07-17 22:13:34 -0500

    docu correct

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
+"""Starts Flask web app
+Listening on 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/', strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_route():
-    '''
-    Route handler for the root URL '/'. Returns a greeting message.
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
-    Returns:
-        str: A string containing the greeting message.
-    '''
-    return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    '''
-    Main entry point of the application.
-    Starts the Flask web server.
-
-    Note:
-        This section will only execute when the script is directly run, not when it is imported as a module.
-    '''
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
