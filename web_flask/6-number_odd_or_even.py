e7e3dd7c5d6730ecab401132e3880b41e4dcab29
ashly922 <ashly922@yahoo.com>
2023-07-18 12:35:00 -0500

    even or odd route added

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Listening on 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
+Route '/hbnb' displays "HBNB"
+Route '/c/<text>' displays c then the text variable
+Route '/python/<text>' displays python followed by the text
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''displays "HBNB"'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''replace underscore w space'''
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    '''pthon followed by text'''
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    '''integer number route def'''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''routes to html page'''
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
+    
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_or_odd(n):
+    '''states if even or odd number'''
+    if isinstance(n, int):
+        return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
