b5d0773c8a3d8194b86fd40c1089f8cd6296738d
ashly922 <ashly922@yahoo.com>
2023-07-18 21:16:57 -0500

    atempt at static

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, g
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    cities = sorted(storage.all(City).values(), key=lambda c: c.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda a: a.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
